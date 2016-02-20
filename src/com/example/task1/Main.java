b6b3815a0a5daa489084592998e84bc8d8fc2d7b
bavenka <bavenka@gmail.com>
2016-02-20 19:58:26 +0300

    Initial commit

---
+++ src/com/example/task1/Main.java
@@ -0,0 +1,7 @@
+package com.example.task1;
+
+public class Main {
+    public static void main(String[] args){
+        Read.read(args[0],Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+    }
+}
